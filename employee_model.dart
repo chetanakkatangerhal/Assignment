6628550a194f067b2ab11cd71961b8c24fbd96b5
chetanakkatangerhal <78865519+chetanakkatangerhal@users.noreply.github.com>
2023-06-29 18:16:10 +0530

    Add files via upload

---
+++ employee_model.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class EmployeeModel {
+  final String? name;
+  final String? address;
+
+  EmployeeModel({
+    @required this.name,
+    @required this.address,
+  });
+}
